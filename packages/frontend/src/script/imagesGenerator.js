a2996938db80b5d17a64a1253b5b485b93f67d26
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2022-08-10 14:36:41 +0200

    Fix images names

--- packages/frontend/src/script/imagesGenerator.js
+++ packages/frontend/src/script/imagesGenerator.js
@@ -13,7 +13,12 @@ const imgRequires = [];
  */
 function generateName(filename) {
     const paths = filename.split("/");
-    return paths[paths.length - 1].split(".")[0].toLowerCase().replace(/ |-/g, "_");
+    return paths[paths.length - 1]
+        .split(".")[0]
+        .replace(/^./, (x) => x.toLowerCase())
+        .replace(/[A-Z]/g, (x) => "_" + x)
+        .toLowerCase()
+        .replace(/ |-/g, "_");
 }
 
 /**
