7ce854fc1093fd259dde5d13cb79daba1cc6e75e
AdriaCarrera <adria.cm95@gmail.com>
2022-09-19 16:09:59 +0200

    Dynamic workspace creation

--- .terraform/staging/main.tf
+++ .terraform/staging/main.tf
@@ -3,7 +3,7 @@ terraform {
         hostname     = "app.terraform.io"
         organization = "Peersyst"
         workspaces {
-            tags = ["staging"]
+            name = "__WORKSPACE__"
         }
     }
 
