f444896478f5033b0b6cc939312ccb3effa0a802
AdriaCarrera <adria.cm95@gmail.com>
2022-09-21 21:30:34 +0200

    Fix staging docker

--- infra/staging/dns.tf
+++ infra/staging/dns.tf
@@ -4,7 +4,7 @@ data "cloudflare_zone" "peersyst-tech" {
 
 resource "cloudflare_record" "www" {
     zone_id = data.cloudflare_zone.peersyst-tech.id
-    name    = "staging.${var.project-name}"
+    name    = var.project-name
     value   = aws_instance.server.public_ip
     type    = "A"
     proxied = true
