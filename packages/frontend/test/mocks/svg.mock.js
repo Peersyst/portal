ff210b9ae85291233ab6e622291596b1007635c3
AgustinMJ <74896585+AgustinMJ@users.noreply.github.com>
2023-03-27 12:42:28 +0200

    Fix svg mock

--- packages/frontend/test/mocks/svg.mock.js
+++ packages/frontend/test/mocks/svg.mock.js
@@ -1,1 +1,6 @@
-module.exports = { ReactComponent: "IconMock" };
+import { forwardRef } from "react";
+
+const SvgMock = forwardRef((props, ref) => <span ref={ref} {...props} />);
+
+export const ReactComponent = SvgMock;
+export default SvgMock;
