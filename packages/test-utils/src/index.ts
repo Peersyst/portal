d7bc835ef8ad866e6fcb4cc81e2f512c4bafc7ff
Longbo <longboqiu@hotmail.com>
2024-07-30 09:41:50 +0200

    feat: add `test-utils` package

---
+++ packages/test-utils/src/index.ts
@@ -0,0 +1,6 @@
+export * from "./createGlobalMock";
+export * from "./MethodMock";
+export * from "./createMock";
+export * from "./Mock";
+export * from "./mockify";
+export * from "./utils/deepmerge";
