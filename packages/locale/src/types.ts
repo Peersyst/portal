c3d818d5119eca28f381594ed66439047e8d54d5
Longbo <longboqiu@hotmail.com>
2024-08-01 16:52:06 +0200

    [PLO] feat: add errors resource

--- packages/locale/src/types.ts
+++ packages/locale/src/types.ts
@@ -4,5 +4,6 @@ import { resources } from "./resources";
 export type LocaleType = Locale;
 export type LocaleResource = (typeof resources)["en"];
 export type LocaleNamespace = keyof LocaleResource;
+export type LocaleErrorResource = keyof LocaleResource["error"];
 export type LocaleTranslationResource = keyof LocaleResource["translation"];
 export type LocaleLanguageResource = keyof LocaleResource["language"];
