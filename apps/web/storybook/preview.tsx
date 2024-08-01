e1c3c52250aa6a3b5a1c32e2221ee325eb1d5120
AgustinMJ <agustinmillanjimenez+github@gmail.com>
2024-08-01 12:06:38 +0200

    [WEB] fix: storybook

--- apps/web/storybook/preview.tsx
+++ apps/web/storybook/preview.tsx
@@ -1,11 +1,10 @@
-import "../src/common/polyfills";
-import "../src/domain/setup";
+import "@/common/polyfills";
+import "@/domain/setup";
 
 import type { Preview } from "@storybook/react";
 import { ToastProvider, ModalProvider } from "@peersyst/react-components";
-import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import Providers from "../src/ui/Providers";
+import Providers from "@/ui/Providers";
 
 const preview: Preview = {
     parameters: {
