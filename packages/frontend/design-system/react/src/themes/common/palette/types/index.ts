ae734030fba7cd8b82beadb93c95ee081bda6b49
Longbo <longboqiu@hotmail.com>
2024-08-23 11:06:44 +0200

    [DS] fix: delete unnecessary type declarations

--- packages/frontend/design-system/react/src/themes/common/palette/types/index.ts
+++ packages/frontend/design-system/react/src/themes/common/palette/types/index.ts
@@ -1,19 +1,6 @@
 import { ThemePalette } from "@peersyst/react-components";
-import { GeneralPalette } from "@frontend/design-system-core/palette";
 import { ButtonPalette } from "./button";
 
 export interface SpecificPalette extends Pick<ThemePalette, "text" | "background" | "backdrop" | "disabled" | "status"> {
     button: ButtonPalette;
 }
-
-export type BasePalette = GeneralPalette;
-
-export type LightPalette = Omit<ThemePalette, "mode"> & {
-    mode: "light";
-};
-
-export type BaseThemePalette<T extends ThemePalette> = GeneralPalette &
-    Pick<ThemePalette, "text" | "background" | "backdrop" | "disabled" | "status"> &
-    Pick<T, "mode">;
-
-export type BaseLightPalette = BaseThemePalette<LightPalette>;
