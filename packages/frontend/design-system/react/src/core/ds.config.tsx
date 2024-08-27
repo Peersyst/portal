5705ee802e9d50704fcb68aa06804eb9eaf68617
Longbo <longboqiu@hotmail.com>
2024-08-27 21:09:47 +0200

    [DS] fix: add missing ds config import

--- packages/frontend/design-system/react/src/core/ds.config.tsx
+++ packages/frontend/design-system/react/src/core/ds.config.tsx
@@ -1,5 +1,5 @@
 import { CreateConfig } from "@peersyst/react-components";
 
-export const config: Partial<CreateConfig> = {
+export const dsConfig: Partial<CreateConfig> = {
     components: {},
 };
