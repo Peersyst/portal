e91dfb46996f0b1b58a6c9c2a34c1faa3946e30b
Agustín Millán Jiménez <74896585+AgustinMJ@users.noreply.github.com>
2024-09-02 14:35:33 +0200

    [SH-TSU] fix: builds

--- packages/frontend/settings/src/ui/query/use-get-settings.ts
+++ packages/frontend/settings/src/ui/query/use-get-settings.ts
@@ -1,4 +1,4 @@
-import { useQuery, UseQueryResult } from "@frontend/query/react";
+import { useQuery, UseQueryResult } from "@tanstack/react-query";
 import { Settings } from "../../common";
 import { getInstance } from "@frontend/core/common/utils/singleton";
 import { SettingsController } from "../../domain/controllers";
