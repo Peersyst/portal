e1622b749ec4e4b96eead03aa1c84f05e86bf491
Jordi Parra Crespo <jordiparra99@gmail.com>
2024-09-04 10:41:30 +0200

    [FE-COR] feat(error): add repository and service error

---
+++ packages/frontend/core/src/data-access/service/error/is-service-error.ts
@@ -0,0 +1,5 @@
+import ServiceError from "./service-error";
+
+export function isServiceError(error: any): error is ServiceError {
+    return error instanceof Error && error.name === "ServiceError";
+}
