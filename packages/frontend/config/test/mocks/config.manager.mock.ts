47711ef7fccade6e0a0284b1aa85688439c3a1e6
Agustín Millán Jiménez <74896585+AgustinMJ@users.noreply.github.com>
2024-09-05 12:09:01 +0200

    [FE-CFG] fix: `config.manager.mock`

--- packages/frontend/config/test/mocks/config.manager.mock.ts
+++ packages/frontend/config/test/mocks/config.manager.mock.ts
@@ -1,27 +1,27 @@
 import { getAttribute } from "@peersyst/react-utils";
-import { EventEmitter } from "@frontend/events";
 import { NestedKeys, DeepPick } from "@swisstype/essential";
-import { Config, config, IConfigManager, IConfigManagerEvents } from "../../src";
+import { Config, config, IConfigManager } from "../../src";
+import { createMock, MethodMock } from "@shared/test";
 
-export class ConfigManagerMock implements IConfigManager {
-    config = config;
-    initialization = Promise.resolve();
-    isLoaded = true;
-    isOutdated = false;
-
-    on: EventEmitter<IConfigManagerEvents>["on"] = jest.fn(() => jest.fn());
-    once: EventEmitter<IConfigManagerEvents>["once"] = jest.fn(() => jest.fn());
-
-    reload = jest.fn((): Promise<void> => Promise.resolve());
-
-    getAll = jest.fn((): Config => config);
-
-    getAllAsync = jest.fn((): Promise<Config> => Promise.resolve(config));
-
-    get = jest.fn(<K extends NestedKeys<Config>>(key: K): DeepPick<Config, K> => getAttribute(config, key) as DeepPick<Config, K>);
-
-    getAsync = jest.fn(
-        <K extends NestedKeys<Config>>(key: K): Promise<DeepPick<Config, K>> =>
-            Promise.resolve(getAttribute(config, key) as DeepPick<Config, K>),
-    );
-}
+export const ConfigManagerMock = createMock<IConfigManager>({
+    config,
+    initialization: Promise.resolve(),
+    isLoaded: true,
+    isOutdated: false,
+    on: new MethodMock("mockReturnValue", jest.fn()),
+    once: new MethodMock("mockReturnValue", jest.fn()),
+    reload: new MethodMock("mockResolvedValue"),
+    getAll: new MethodMock("mockReturnValue", config),
+    getAllAsync: new MethodMock("mockResolvedValue", config),
+    get: new MethodMock(
+        "mockImplementation",
+        jest.fn(<K extends NestedKeys<Config>>(key: K): DeepPick<Config, K> => getAttribute(config, key) as DeepPick<Config, K>),
+    ),
+    getAsync: new MethodMock(
+        "mockImplementation",
+        jest.fn(
+            <K extends NestedKeys<Config>>(key: K): Promise<DeepPick<Config, K>> =>
+                Promise.resolve(getAttribute(config, key) as DeepPick<Config, K>),
+        ),
+    ),
+});
