d0e3938f067e716f693bb5961de5e22bc660a2cb
Agustín Millán Jiménez <74896585+AgustinMJ@users.noreply.github.com>
2024-09-06 12:04:25 +0200

    [FR-COR] feat: domain errors

---
+++ packages/frontend/core/src/domain/error/decorators/repository-errors.ts
@@ -0,0 +1,18 @@
+import { isRepositoryError } from "../../../data-access/repository/error";
+import { createErrorHandlerDecorator } from "../../decorator";
+import { DomainError, DomainErrorOptions } from "../domain-error";
+
+function handleRepositoryError(error: any, errors: Record<string, string | DomainErrorOptions>) {
+    if (isRepositoryError(error)) {
+        const domainError = errors[error.message];
+        throw typeof domainError === "string"
+            ? new DomainError(domainError)
+            : DomainError.fromOptions({ ...domainError, data: { ...error.data, ...domainError.data } });
+    } else throw error;
+}
+
+/**
+ * Decorator to handle repository errors
+ * @param errors A map of error messages to domain errors `{ USER_NOT_FOUND: UserErrors.USER_NOT_FOUND }`
+ */
+export const RepositoryErrors = createErrorHandlerDecorator(handleRepositoryError);
