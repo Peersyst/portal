6d5d1eb4b6ae52f2d91107c49422d28416228c9a
Agustín Millán Jiménez <74896585+AgustinMJ@users.noreply.github.com>
2024-09-08 13:58:03 +0200

    [FR-SET] chore: add `jsdoc`

--- packages/frontend/settings/src/ui/state/use-settings-state.ts
+++ packages/frontend/settings/src/ui/state/use-settings-state.ts
@@ -2,4 +2,8 @@ import { useStore } from "zustand";
 import { settingsState } from "../../domain/settings.state";
 import { Settings } from "../../common";
 
+/**
+ * Gets the settings state.
+ * @returns The settings state.
+ */
 export const useSettingsState = (): Settings => useStore(settingsState);
