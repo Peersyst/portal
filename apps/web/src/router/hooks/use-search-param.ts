47bf6a4550b05c97634cc55beec6a2add9f99858
Agustín Millán Jiménez <74896585+AgustinMJ@users.noreply.github.com>
2024-09-08 13:02:21 +0200

    [WEB] feat: add `router` hooks

---
+++ apps/web/src/router/hooks/use-search-param.ts
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+
+export interface UseSearchParamOptions<T = string> {
+    parse?: (value: string) => T;
+    validation?: (value: string) => boolean;
+    fallback?: T;
+}
+
+/**
+ * @param key - The search parameter key.
+ * @param options - The options for the search parameter.
+ * @returns A tuple containing the search parameter and a function to set the search parameter.
+ */
+export function useSearchParam<T = string>(
+    key: string,
+    { parse = (value: string) => value as T, validation, fallback }: UseSearchParamOptions<T> = {},
+): [T | undefined, (value: T | undefined) => void] {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [searchParamValue, setSearchParamValue] = useState<string | undefined>();
+
+    useEffect(() => {
+        const value = searchParams.get(key) || undefined;
+        if (value && validation && !validation(value)) setSearchParam(fallback);
+        else setSearchParamValue(value);
+    }, [searchParams.get(key)]);
+
+    const setSearchParam = (value: T | undefined) => {
+        if (value) searchParams.set(key, value.toString());
+        else searchParams.delete(key);
+        setSearchParams(searchParams);
+    };
+
+    return [searchParamValue ? parse(searchParamValue) : undefined, setSearchParam];
+}
