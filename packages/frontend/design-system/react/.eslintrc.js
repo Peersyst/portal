7a6c4198f4e14952521ef4d42440490b8151b5ec
Agustín Millán Jiménez <74896585+AgustinMJ@users.noreply.github.com>
2024-09-09 16:17:57 +0200

    [WEB] fix: testing

--- packages/frontend/design-system/react/.eslintrc.js
+++ packages/frontend/design-system/react/.eslintrc.js
@@ -1,3 +1,11 @@
 module.exports = {
     extends: [require.resolve("@shared/eslint/react")],
+    overrides: [
+        {
+            files: ["storybook/**/*"],
+            rules: {
+                "jsdoc/require-jsdoc": "off",
+            },
+        },
+    ],
 };
