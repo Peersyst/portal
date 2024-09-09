07698215696d9b9b09237a8bd5aab7c10db1b3bf
Agustín Millán Jiménez <74896585+AgustinMJ@users.noreply.github.com>
2024-09-09 18:03:36 +0200

    [WEB][MOBILE] chore: fix test setups

--- apps/mobile/test/setup.tsx
+++ apps/mobile/test/setup.tsx
@@ -21,7 +21,7 @@ jest.mock("@aws-sdk/client-appconfigdata", () => require.resolve("@aws-sdk/clien
 
 // Mock `configManager`
 jest.mock("../src/core/config", () => {
-    const { ConfigManagerMock } = require("@frontend/config/test/mocks");
+    const { ConfigManagerMock } = require("@frontend/config/mocks/core/manager");
 
     return {
         configManager: new ConfigManagerMock(),
