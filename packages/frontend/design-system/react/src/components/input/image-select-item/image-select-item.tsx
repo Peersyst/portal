08b2274e75b0570949aae57c884ae1aebd6fc25a
Agustín Millán Jiménez <74896585+AgustinMJ@users.noreply.github.com>
2024-09-26 17:45:31 +0200

    [root] chore: replace `css` for `style`

--- packages/frontend/design-system/react/src/components/input/image-select-item/image-select-item.tsx
+++ packages/frontend/design-system/react/src/components/input/image-select-item/image-select-item.tsx
@@ -2,9 +2,9 @@ import { Row, Typography } from "@peersyst/react-components";
 import { ImageSelectItemProps } from "./image-select-item.types";
 import { SelectItemImage } from "./image-select-item.styles";
 
-export function ImageSelectItem({ src, alt, label, style, className }: ImageSelectItemProps): JSX.Element {
+export function ImageSelectItem({ src, alt, label, style = {}, className }: ImageSelectItemProps): JSX.Element {
     return (
-        <Row flex={1} style={{ overflow: "hidden" }} gap="0.5rem" alignItems="center" style={style} className={className}>
+        <Row flex={1} style={{ overflow: "hidden", ...style }} gap="0.5rem" alignItems="center" className={className}>
             <SelectItemImage src={src || ""} alt={alt || `${label} logo`} />
             {typeof label === "string" ? (
                 <Typography style={{ flex: 1, textOverflow: "ellipsis" }} variant="body2Regular">
