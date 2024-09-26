a950459b3c4620b639c9412f8fe7f9f3255dc2a0
Agustín Millán Jiménez <74896585+AgustinMJ@users.noreply.github.com>
2024-09-26 15:46:52 +0200

    [web] fix: axelar transfer tweaks

--- packages/frontend/bridge/src/domain/controllers/bride-transfer/bridge-transfer.controller.ts
+++ packages/frontend/bridge/src/domain/controllers/bride-transfer/bridge-transfer.controller.ts
@@ -206,11 +206,11 @@ export class BridgeTransferController implements IBridgeTransferController {
             this.eventEmitter.emit("awaitReceiptStarted");
             this.eventEmitter.emit("status", BridgeTransferStatus.SENT);
 
-            await new Promise((resolve) => setTimeout(resolve, 30000));
+            await new Promise((resolve) => setTimeout(resolve, 40000));
 
             this.eventEmitter.emit("status", BridgeTransferStatus.CONFIRMED);
 
-            await new Promise((resolve) => setTimeout(resolve, 30000));
+            await new Promise((resolve) => setTimeout(resolve, 40000));
 
             this.eventEmitter.emit("status", BridgeTransferStatus.RECEIVED);
             this.eventEmitter.emit("awaitReceiptCompleted");
