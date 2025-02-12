755f3a40ae3f5582e5e07ad10c7451ba58a27a39
Agustín Millán Jiménez <74896585+AgustinMJ@users.noreply.github.com>
2025-02-12 18:27:56 +0100

    [web] chore: "release" fixes

--- packages/frontend/bridge/src/domain/controllers/bride-transfer/bridge-transfer.controller.ts
+++ packages/frontend/bridge/src/domain/controllers/bride-transfer/bridge-transfer.controller.ts
@@ -208,11 +208,11 @@ export class BridgeTransferController implements IBridgeTransferController {
             this.eventEmitter.emit("awaitReceiptStarted");
             this.eventEmitter.emit("status", BridgeTransferStatus.SENT);
 
-            await new Promise((resolve) => setTimeout(resolve, 10000));
+            await new Promise((resolve) => setTimeout(resolve, 75000));
 
             this.eventEmitter.emit("status", BridgeTransferStatus.CONFIRMED);
 
-            await new Promise((resolve) => setTimeout(resolve, 10000));
+            await new Promise((resolve) => setTimeout(resolve, 75000));
 
             this.eventEmitter.emit("status", BridgeTransferStatus.RECEIVED);
             this.eventEmitter.emit("awaitReceiptCompleted");
